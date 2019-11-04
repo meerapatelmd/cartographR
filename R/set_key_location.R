38eaa5009011495aab4da7ee52243649681c4638
patelm9 <patelm9@mskcc.org>
2019-11-04 13:48:30 -0500

    update function

--- R/set_key_location.R
+++ R/set_key_location.R
@@ -4,45 +4,39 @@
 #' @importFrom mirCat list_add
 #' @export
 
-set_key_location <-
-        function(path, is.gsheet = TRUE, file_path = !is.gsheet) {
-                if (is.gsheet == TRUE) {
-                        gsheet_path <- path
-                        names(gsheet_path) <- c("gsheet_path")
+set_paths <-
+        function(new_catalogue_path = NULL, new_key_path = NULL) {
+                setClass("cartograph", representation(source = "character", age = "numeric"), 
+                         prototype(name = NA_character_, age = NA_real_))
+                if (!file.exists("CARTOGRAPH.RData")) {
+                        CARTOGRAPH <- list()
+                        if (is.null(new_catalogue_path)) {
+                                CARTOGRAPH[[1]] <- "/Users/meerapatel/GitHub/MSK_KMI_Enterprise/TerminologyBiblioTech/CATALOGUE"
+                        } else {
+                                CARTOGRAPH[[1]] <- new_catalogue_path
+                        }
                         
-                        if (!exists("CARTOGRAPH", envir = globalenv())) {
-                                x <- list()
-                                x[[1]] <- gsheet_path
-                                names(x) <- "KEY_LOCATION"
-                                
-                                assign("CARTOGRAPH", value = x, envir = globalenv())
+                        if (is.null(new_key_path)) {
+                                CARTOGRAPH[[2]] <- "/Users/meerapatel/GitHub/MSK_KMI_Enterprise/TerminologyBiblioTech/KEY"
                         } else {
-                                cartograph <- CARTOGRAPH
-                                if ("KEY_LOCATION" %in% names(cartograph)) {
-                                        cartograph[["KEY_LOCATION"]] <- gsheet_path
-                                        assign("CARTOGRAPH", value = cartograph, envir = globalenv())
-                                } else {
-                                        cartograph <- mirCat::list_add(cartograph, gsheet_path, name = "KEY_LOCATION")
-                                        assign("CARTOGRAPH", value = cartograph, envir = globalenv())
-                                }
+                                CARTOGRAPH[[2]] <- new_key_path
                         }
+                        names(CARTOGRAPH) <- c("CATALOGUE_PATH", "KEY_PATH")
+                        mirroR::save_robj(CARTOGRAPH)
+                        assign("CARTOGRAPH", CARTOGRAPH, envir = globalenv())
                 } else {
-                        names(path) <- "file_path"
-                        if (!exists("CARTOGRAPH", envir = globalenv())) {
-                                x <- list()
-                                x[[1]] <- path
-                                names(x) <- "KEY_LOCATION"
-                                
-                                assign("CARTOGRAPH", value = x, envir = globalenv())
-                        } else {
-                                cartograph <- CARTOGRAPH
-                                if ("KEY_LOCATION" %in% names(cartograph)) {
-                                        cartograph[["KEY_LOCATION"]] <- path
-                                        assign("CARTOGRAPH", value = cartograph, envir = globalenv())
-                                } else {
-                                        cartograph <- mirCat::list_add(cartograph, path, name = "KEY_LOCATION")
-                                        assign("CARTOGRAPH", value = cartograph, envir = globalenv())
-                                }
+                        CARTOGRAPH <- readRDS("CARTOGRAPH.RData")
+                        if (!is.null(new_catalogue_path)) {
+                                CARTOGRAPH[["CATALOGUE_PATH"]] <- new_catalogue_path
+                                mirroR::save_robj(CARTOGRAPH)
+                                assign("CARTOGRAPH", CARTOGRAPH, envir = globalenv())
                         }
+                        
+                        if (!is.null(new_key_path)) {
+                                CARTOGRAPH[["KEY_PATH"]] <- new_key_path
+                                mirroR::save_robj(CARTOGRAPH)
+                                assign("CARTOGRAPH", CARTOGRAPH, envir = globalenv())
+                        }
+                        
                 }
         }
