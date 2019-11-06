2eb134e6f86b24eb1a9ff29d621d42cb00d4185f
patelm9 <patelm9@mskcc.org>
2019-11-06 14:21:11 -0500

    bug fix: write_mrconso_net

--- R/write_mrconso_net.R
+++ R/write_mrconso_net.R
@@ -21,35 +21,42 @@ write_mrconso_net <-
                 }
                 
                 exact_phrase_file_ext <- paste0("[0-9]{12}[_]{1}", phrase_01, "[.]{1}csv$")
-                exact_phrase <- paste0("^", phrase_01, "$")
                 
-                if (!(exact_phrase %in% glossary$UMLS_SQL_KEYWORD)) {
+                if (cast_wide_net == TRUE) {
+                        phrase_02 <- phrase_01
+                } else {
+                        phrase_02 <- paste0("^", phrase_01, "$")
+                }
+                
+                if (any(grepl(exact_phrase_file_ext, files_list)) == TRUE) {
                                         projektoR::append_csv(glossary_fn,
                                                               dataframe = data.frame(
                                                                       UMLS_SQL_KEYWORD_TIMESTAMP = mirroR::get_timestamp(),
                                                                       UMLS_SQL_KEYWORD_ID = as.character(max(as.double(glossary$UMLS_SQL_KEYWORD_ID)) + 1),
-                                                                      UMLS_SQL_KEYWORD = phrase_01
+                                                                      UMLS_SQL_KEYWORD = phrase_01 #Not `phrase_02` because the regex will not be compatible with SQL QUERIES
                                                               ))
                                         
                                         glossary <- readr::read_csv(glossary_fn, col_types = cols(.default = "c"))
                                         keyword_id <- glossary %>%
                                                                 dplyr::filter(UMLS_SQL_KEYWORD == phrase_01) %>%
                                                                 dplyr::select(UMLS_SQL_KEYWORD_ID) %>%
+                                                                dplyr::filter(row_number() == 1) %>%
                                                                 unlist() %>%
                                                                 unname()
                                         
                                         
-                                        sql_statement <- paste0("SELECT * FROM MRCONSO WHERE STR LIKE '%", phrase_01, "%';")
+                                        sql_statement <- paste0("SELECT * FROM MRCONSO WHERE STR LIKE '%", phrase_01, "%';") #Not `phrase_02` because the regex will not be compatible with SQL QUERIES
                                         cohort <- mySeagull::get_query("umls", sql_statement)
                                         
                                         output_fn <- mirroR::create_path_to_file(path_folder = "/Users/meerapatel/GitHub/MSK_KMI_Enterprise/TerminologyBiblioTech/CATALOGUE/UMLS_MT_MRCONSO/",
-                                                                          basename = paste0(keyword_id, "_", phrase_01),
+                                                                          basename = paste0(keyword_id, "_", phrase_01), #Not `phrase_02` because the regex will not be compatible with output filenames
                                                                           file_extension = "csv")
                                         
                                         if (!(file.exists(output_fn))) {
                                                 readr::write_csv(cohort, path = output_fn)
                                         } else {
-                                                typewriteR::tell_me(output_fn, "already exists. Overwrite? ")
+                                                typewriteR::tell_me("\t", output_fn, "already exists. Overwrite? ")
+                                                cat("\n")
                                                 typewriteR::stop_and_enter()
                                                 readr::write_csv(cohort, path = output_fn)
                                         }
