e5985299ecd6377447f06d43dc087a5009249c5b
patelm9 <patelm9@mskcc.org>
2019-11-06 10:47:34 -0500

    bug fix: double_query_mrconso

--- R/double_query_mrconso.R
+++ R/double_query_mrconso.R
@@ -3,8 +3,15 @@
 #' @export
 #' 
 double_query_mrconso <-
-        function(search_word_01, search_word_02) {
-                                        sql_statement <- paste0("SELECT CUI, STR FROM MRCONSO WHERE STR LIKE '%", search_word_01, "%' AND STR LIKE '%", search_word_02, "%' AND LAT = 'ENG' AND ISPREF = 'Y' LIMIT 1;")
-                                        cohort <- mySeagull::get_query("umls", sql_statement)
-                                        return(cohort)
+        function(search_word_01, search_word_02, limit_1 = FALSE) {
+                                        if (limit_1 == TRUE) {
+                                                sql_statement <- paste0("SELECT CUI, STR FROM MRCONSO WHERE STR LIKE '%", search_word_01, "%' AND STR LIKE '%", search_word_02, "%' AND LAT = 'ENG' AND ISPREF = 'Y' 
+                                                                LIMIT 1;")
+                                                cohort <- mySeagull::get_query("umls", sql_statement)
+                                                return(cohort)
+                                        } else {
+                                                sql_statement <- paste0("SELECT CUI, STR FROM MRCONSO WHERE STR LIKE '%", search_word_01, "%' AND STR LIKE '%", search_word_02, "%' AND LAT = 'ENG' AND ISPREF = 'Y';")
+                                                cohort <- mySeagull::get_query("umls", sql_statement)
+                                                return(cohort)
+                                        }
                                 }
