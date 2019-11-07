5049229ae069de388e4515b4fd3cedc4affc9846
patelm9 <patelm9@mskcc.org>
2019-11-07 11:01:18 -0500

    bug fix: jatpat_mrconso_coordinate

--- R/jatpat_mrconso_coordinate.R
+++ R/jatpat_mrconso_coordinate.R
@@ -21,19 +21,24 @@ jatpat_mrconso_coordinate <-
                         start_index <- 1
                 } else {
                         output_dataframe <- get(output_dataframe_name, envir = globalenv())
-                        start_index <- nrow(output_dataframe)
+                        
+                        if (nrow(output_dataframe) == 0) {
+                                start_index <- 1
+                        } else {
+                                start_index <- 1 + nrow(output_dataframe)
+                        }
                 }
                 
                 ##Preparing arugments
                 sql_term_col <- enquo(sql_term_col)
                 
                 ##Getting glossary
-                glossary_fn <- "/Users/meerapatel/GitHub/MSK_KMI_Enterprise/TerminologyBiblioTech/CATALOGUE/UMLS_MT_MRCONSO/GLOSSARY_UMLS_MT_MRCONSO_QUERY_TERMS.csv"
-                glossary <- readr::read_csv(glossary_fn, col_types = cols(.default = "c")) 
+                # glossary_fn <- "/Users/meerapatel/GitHub/MSK_KMI_Enterprise/TerminologyBiblioTech/CATALOGUE/UMLS_MT_MRCONSO/GLOSSARY_UMLS_MT_MRCONSO_QUERY_TERMS.csv"
+                # glossary <- readr::read_csv(glossary_fn, col_types = cols(.default = "c")) 
                 
                 ##Filtering glossary for coordinates only
-                coordinate_glossary <- glossary %>%
-                                        dplyr::filter(UMLS_SQL_KEYWORD_CLASS == "coordinate")
+                # coordinate_glossary <- glossary %>%
+                #                         dplyr::filter(UMLS_SQL_KEYWORD_CLASS == "coordinate")
                 
                 ##Preparing dataframe
                 dataframe <-
@@ -46,7 +51,7 @@ jatpat_mrconso_coordinate <-
                 ##Anti-joining dataframe to get the values that aren't in the glossary
                 new_terms_df <-
                         dataframe %>%
-                        dplyr::anti_join(coordinate_glossary, by = "UMLS_SQL_KEYWORD") %>%
+                        #dplyr::anti_join(coordinate_glossary, by = "UMLS_SQL_KEYWORD") %>%
                         dplyr::mutate(COORDINATE_CUI = "") %>%
                         dplyr::mutate(COORDINATE_STR = "")
                 
@@ -57,18 +62,19 @@ jatpat_mrconso_coordinate <-
                                 }
                                 
                                 ##Updating glossary
-                                typewriteR::tell_me(Sys.time(), "Updating glossary with", i, "of", total_obs)
-                                cat("\n")
-                                projektoR::append_csv(glossary_fn,
-                                                      dataframe = data.frame(
-                                                              UMLS_SQL_KEYWORD_TIMESTAMP = mirroR::get_timestamp(),
-                                                              UMLS_SQL_KEYWORD_ID = as.character(max(as.double(glossary$UMLS_SQL_KEYWORD_ID)) + 1),
-                                                              UMLS_SQL_KEYWORD = new_terms_df$UMLS_SQL_KEYWORD[i],
-                                                              UMLS_SQL_KEYWORD_CLASS = "coordinate"
-                                                      ))
+                                # typewriteR::tell_me(Sys.time(), "Updating glossary with", i, "of", total_obs)
+                                # cat("\n")
+                                # projektoR::append_csv(glossary_fn,
+                                #                       dataframe = data.frame(
+                                #                               UMLS_SQL_KEYWORD_TIMESTAMP = mirroR::get_timestamp(),
+                                #                               UMLS_SQL_KEYWORD_ID = as.character(max(as.double(glossary$UMLS_SQL_KEYWORD_ID)) + 1),
+                                #                               UMLS_SQL_KEYWORD = new_terms_df$UMLS_SQL_KEYWORD[i],
+                                #                               UMLS_SQL_KEYWORD_CLASS = "coordinate"
+                                #                       ))
                                 
                                 
                                 ##Jatpat
+                                cat("\n")
                                 typewriteR::tell_me(Sys.time(), "Querying MRCONSO STR for", new_terms_df$UMLS_SQL_KEYWORD[i])
                                 cat("\n")
                                 mrconso_data <- query_mrconso_data_exact_limit_one(new_terms_df$UMLS_SQL_KEYWORD[i])
@@ -80,19 +86,27 @@ jatpat_mrconso_coordinate <-
                                         new_terms_df$COORDINATE_STR[i] <- NA
                                 }
                                 typewriteR::tell_me(Sys.time(), "Finished querying MRCONSO STR for", new_terms_df$UMLS_SQL_KEYWORD[i])
+                                cat("\n")
+                                
+                                ##Updating output_dataframe
+                                output_dataframe <- dplyr::bind_rows(output_dataframe, 
+                                                                     new_terms_df %>%
+                                                                             dplyr::filter(row_number() == i)
+                                )
                                 
                                 ##Saving RDS
-                                output_dataframe <- new_terms_df
+                                cat("\n")
                                 rds_fn <- paste0(script_step_number, "_", output_dataframe_name, ".RData")
                                 saveRDS(output_dataframe, file = rds_fn)
                                 typewriteR::tell_me(Sys.time(), rds_fn, "saved...")
                                 cat("\n")
                                 
                                 ##Concurrent to saving RDS, assigning robject to global
+                                cat("\n")
                                 assign(output_dataframe_name, output_dataframe, envir = globalenv())
                                 typewriteR::tell_me(output_dataframe_name, "R object updated...")
                                 cat("\n")
-                                
+                                typewriteR::tell_me(i, "of", total_obs, "completed.")
                                 cat("\n\n\n")
                                 
                         }
