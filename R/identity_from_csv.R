ba996ac749073cbeea8d59ac2b1754efd6a9e282
patelm9 <patelm9@mskcc.org>
2019-11-09 00:54:25 -0500

    bug fix: identity_from_csv

--- R/identity_from_csv.R
+++ R/identity_from_csv.R
@@ -16,51 +16,62 @@
 identity_from_csv <-
         function(path_to_identity,
                  target_cols,
+                 redcap_forms_to_exclude = NULL, 
                  keep_all_cols = FALSE,
                  dont_trimws = FALSE,
                  remove_all = "\t|\n",
                  include_duplicates = FALSE,
                  log = TRUE) {
                 
-                IDENTITY_TARGET_COLS <- target_cols
+                IDENTITY_TARGET_COLS <- c("IDENTITY_ID", target_cols)
                 IDENTITY_00 <- readr::read_csv(path_to_identity, col_types = readr::cols(.default = "c"))
                 
-                if (keep_all_cols == TRUE) {
-                        IDENTITY_01 <- IDENTITY_01
-                } else {
+                if (!is.null(redcap_forms_to_exclude)) {
                         IDENTITY_01 <-
                                 IDENTITY_00 %>%
-                                dplyr::select(IDENTITY_TARGET_COLS)
+                                dplyr::filter_at(vars(FORM_NAME), any_vars(!(. %in% redcap_forms_to_exclude)))
+                } else {
+                        IDENTITY_01 <-
+                                IDENTITY_00 
                 }
                 
-                if (dont_trimws == TRUE) {
+                if (keep_all_cols == TRUE) {
                         IDENTITY_02 <- IDENTITY_01
                 } else {
                         IDENTITY_02 <-
                                 IDENTITY_01 %>%
+                                dplyr::select(IDENTITY_TARGET_COLS)
+                }
+                
+                if (dont_trimws == TRUE) {
+                        IDENTITY_03 <- IDENTITY_02
+                } else {
+                        IDENTITY_03 <-
+                                IDENTITY_02 %>%
                                 dplyr::mutate_all(trimws, "both")
                 }
                 
                 if (remove_all == ""|is.null(remove_all)) {
-                        IDENTITY_03 <-
-                                IDENTITY_02 
+                        IDENTITY_04 <-
+                                IDENTITY_03
                 } else {
-                        IDENTITY_03 <-
-                                IDENTITY_02 %>%
+                        IDENTITY_04 <-
+                                IDENTITY_03 %>%
                                 dplyr::mutate_all(stringr::str_remove_all, remove_all)
                 }
                 
                 
-                IDENTITY_04 <-
-                        reshape2::melt(IDENTITY_03, id.vars = "IDENTITY_ID", variable.name = "KEY_FIELD", value.name = "KEY_CONCEPT_NAME") %>%
+                
+                IDENTITY_05 <-
+                        reshape2::melt(IDENTITY_04, id.vars = "IDENTITY_ID", variable.name = "KEY_FIELD", value.name = "KEY_CONCEPT_NAME") %>%
                         dplyr::mutate_all(as.character) %>%
                         dplyr::mutate(KEY_FIELD = str_replace_all(KEY_FIELD, "^PERMISSIBLE_VALUE_LABEL$", "KMI_PERMISSIBLE_VALUE_LABEL"))
                 
                 if (include_duplicates == TRUE) {
-                        IDENTITY_05 <- IDENTITY_04
+                        IDENTITY_06 <- IDENTITY_05
                 } else {
-                        IDENTITY_05 <-
-                                IDENTITY_04 %>%
+                        IDENTITY_06 <-
+                                IDENTITY_05 %>%
                                 dplyr::distinct()
                 }
                 
@@ -73,5 +84,5 @@ identity_from_csv <-
                                             )
                 }
                 
-                return(IDENTITY_05)
+                return(IDENTITY_06)
         }
